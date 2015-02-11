f153e55a9cf0d4e6e78c4132bd2085955ff4fcc3
wanghuafeng <wachfx@163.com>
2015-02-11 18:59:36 +0800

    update

--- zhihu.py
+++ zhihu.py
@@ -3,6 +3,7 @@ __author__ = 'wanghuafeng'
 import os
 import re
 import time
+import json
 import codecs
 import requests
 from bs4 import BeautifulSoup
@@ -10,14 +11,31 @@ from bs4 import BeautifulSoup
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 class ZhiHu(object):
+
     topic_root_url = 'http://www.zhihu.com/topics'#话题广场根目录
+    root_url = 'http://www.zhihu.com'#根目录
+
     def __init__(self):
-        self.log_record()
+        self.chosen_id_list = []
+        self._load_cat_id()
+
+    def _load_cat_id(self):
+        '''读取本地文件中不以#号开头的id信息，保存到self.chosen_id_list中'''
+        filename = os.path.join(PATH, 'cat_id_mapping.txt')
+        with codecs.open(filename, encoding='utf-8') as f:
+            for line in f.readlines():
+                if not line.startswith('#'):
+                    print line.strip()
+                    topic_id = line.strip().split('\t')[-1]
+                    self.chosen_id_list.append(topic_id)
+            print len(self.chosen_id_list)
+
     def log_record(self):
         '''生成log句柄，用于记录日志'''
         log_filename = time.strftime('%Y_%m_%d.log')
         log_file = os.path.join(PATH, 'log', log_filename)
         self.log = codecs.open(log_file, mode='a', encoding='utf-8')
+
     def gen_all_topic_url_list(self):
         '''解析根目录页面，获取所有话题url'''
         try:
@@ -33,10 +51,41 @@ class ZhiHu(object):
                 return []
         soup = BeautifulSoup(html)
         url_str = soup.find('ul', class_="zm-topic-cat-main clearfix")
-        url_list = url_str.find_all('li', attrs={'data-id':True})
+        url_li_list = url_str.find_all('li', attrs={'data-id':True})
+        print url_li_list
+        url_list = [(item.a.text, item['data-id']) for item in url_li_list]
         print url_list
+        codecs.open('cat_id_mapping.txt', mode='wb', encoding='utf-8').writelines([('\t'.join(item) + '\n') for item in url_list])
+
+    def get_cookie_param(self):
+        '''获取cookie信息'''
+        cookie = requests.get(self.topic_root_url).cookies.get('_xsrf')
+        print 'cookie info: ', cookie
+        return cookie
+
+    def get_cattopic_id(self, cattopic_id):
+        '''由cattopic_id获取topic_id，这里去zhihu默认的请求次数（3次），即每个cattopic_id对应60个topic_id'''
+        topic_url = 'http://www.zhihu.com/node/TopicsPlazzaListV2'
+        total_topic_id_set = set()
+        for offset_index in range(3):
+            post_data = {
+                'method':'next',
+                'params':'{"topic_id":1027,"offset":%d,"hash_id":""}',
+                '_xsrf':'9095d080aa27b6669de39a5a5eb9c439',
+                }
+            post_data['params'] = post_data['params'] % (offset_index*20)
+            json_html = requests.post(topic_url, data=post_data).text
+            json_data = json.loads(json_html)
+            msg_list = json_data.get('msg')
+            print len(msg_list)
+            topic_url_list = [BeautifulSoup(item).find('a')['href'] for item in msg_list]
+            print topic_url_list
+            total_topic_id_set |= set(topic_url_list)
+        print len(total_topic_id_set)
+
 if __name__ == "__main__":
     zhihu = ZhiHu()
     start_time = time.time()
-    zhihu.gen_all_topic_url_list()
+    # zhihu.gen_all_topic_url_list()
+    # zhihu.get_cookie_param()
     print time.time()-start_time
--- zhitst.py
+++ zhitst.py
@@ -1,19 +1,75 @@
 __author__ = 'huafeng'
 #coding:utf-8
 import re
+import requests
 import time
+import json
 import subprocess
-
-html = '''<ul class="zm-topic-cat-main clearfix">
-<li data-id="3324"><a href="#经济学">经济学</a></li>
-<li data-id="253"><a href="#游戏">游戏</a></li>
-</ul>'''
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(html)
-url_list = soup.find_all('li', attrs={'data-id':re.compile('\d+')})
-print url_list
-#当属性为id, class时可以使用id=True, class_=True 来获取有id, class属性的数据（text=True也可以）。
-#如soup.find_all('li', id=True)
-#soup.find_all('li', class_=True)
-#其他属性则必须使用attrs={'data-id':True}来根据制定的属性进行过滤，例子中不能使用'data-id'=True
 
+def parse_url_li():
+    html = '''<ul class="zm-topic-cat-main clearfix">
+    <li data-id="3324"><a href="#经济学">经济学</a></li>
+    <li data-id="253"><a href="#游戏">游戏</a></li>
+    </ul>'''
+    soup = BeautifulSoup(html)
+    url_list = soup.find_all('li', attrs={'data-id':re.compile('\d+')})
+    print url_list
+    #当属性为id, class时可以使用id=True, class_=True 来获取有id, class属性的数据（text=True也可以）。
+    #如soup.find_all('li', id=True)
+    #soup.find_all('li', class_=True)
+    #其他属性则必须使用attrs={'data-id':True}来根据制定的属性进行过滤，例子中不能使用'data-id'=True
+
+def request_page_url():
+    url = u'http://www.zhihu.com/topics'
+    html = requests.get(url).text
+    soup = BeautifulSoup(html)
+    div_level_str = soup.find('div', class_='zh-general-list clearfix')
+    item_level_list = div_level_str.find_all('div', class_='item')
+    print item_level_list
+    print len(item_level_list)
+    print item_level_list[0].a['href']
+# request_page_url()
+
+def post_to_get_topic_id():
+
+    topic_url = 'http://www.zhihu.com/node/TopicsPlazzaListV2'
+    total_topic_id_set = set()
+    for offset_index in range(3):
+        post_data = {
+            'method':'next',
+            'params':'{"topic_id":1027,"offset":%d,"hash_id":""}',
+            '_xsrf':'9095d080aa27b6669de39a5a5eb9c439',
+            }
+        post_data['params'] = post_data['params'] % (offset_index*20)
+        json_html = requests.post(topic_url, data=post_data).text
+        json_data = json.loads(json_html)
+        msg_list = json_data.get('msg')
+        print len(msg_list)
+        topic_url_list = [BeautifulSoup(item).find('a')['href'] for item in msg_list]
+        print topic_url_list
+        total_topic_id_set |= set(topic_url_list)
+    print len(total_topic_id_set)
+# post_to_get_topic_id()
+
+def get_questions_by_topic_id():
+    url = 'http://www.zhihu.com/topic/19553298'
+    r = requests.get(url)
+    html = r.text
+    # print html
+    soup = BeautifulSoup(html)
+    div_level_list = soup.find_all('div', class_='content')
+    print len(div_level_list)
+    print div_level_list
+get_questions_by_topic_id()
+
+def gen_range_offset():
+    for offset_index in range(3):
+        post_data = {
+            'method':'next',
+            'params':'{"topic_id":1027,"offset":%d,"hash_id":""}',
+            '_xsrf':'9095d080aa27b6669de39a5a5eb9c439',
+            }
+        post_data['params'] = post_data['params'] % offset_index
+        print post_data
+# gen_range_offset()
